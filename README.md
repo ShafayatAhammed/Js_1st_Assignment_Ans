9eeb4f6668345e4c493dfb19ce93380c700ffdab
ShafayatAhammed <mdshafayat013@gmail.com>
2024-05-30 22:41:39 +0600

    assignment answers

---
+++ README.md
@@ -0,0 +1,1 @@
+# Js_1st_Assignment_Ans
---
+++ script.js
@@ -0,0 +1,85 @@
+// answer to the question no 1
+
+const carName = "Volvo";
+
+// answer to the question no 2
+
+const firstName = "John";
+const lastName = "Doe";
+const age = 35;
+
+// answer to the question no 3
+
+let x = 10;
+const y = 5;
+x *= y;
+
+// console.log(x);
+
+// answer to the question no 4
+
+let length = 16; // Number Type Data
+// let lastName = "Johnson"; // String Type Data
+
+// const x = {
+//   firstName: "John",  
+//   lastName: "Doe"
+// }; // Object Type Data
+
+// answer to the question no 5
+
+function myFunction() {
+  alert("Hello World!");
+}
+
+// myFunction();
+
+// answer to the question on 6
+
+const person = {
+  name: "John",
+  age: 50
+}
+
+// const {name,age} = person;
+
+// alert(`${name} is ${age}`);
+
+// answer to the question no 7
+
+function clicked(){
+  console.log("The button has clicked");
+}
+
+// answer to the question no 8
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+
+// alert(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+
+Brand[0] = "Ford";
+
+// console.log(Brand);
+
+// answer to the question no 9
+
+// console.log(Math.random());
+
+// console.log(Math.max(10,20));
+
+// console.log(Math.sqrt(9));
+
+// answer to the question no 10
+
+// const x = 10;
+// const y = 5;
+
+// alert(x > y);
+
+// let age;
+
+// const response = (age < 18) ? "Too young" : "Old enough";
+
+// alert(response);
